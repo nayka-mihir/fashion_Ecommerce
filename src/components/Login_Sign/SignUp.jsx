7f43c08b74b12e6e937eb9e313f584d86b2a2f14
nayka-mihir <mihirnayka786@gmail.com>
2026-01-23 11:16:14 +0530

    add Sign up

---
+++ src/components/Login_Sign/SignUp.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+function SignUp({Setshowlogin,Setcurrentstate}) {
+  return (
+    <div className=" absolute z-1 items-center bg-[#fff] top-20 left-150 border">
+      <form action="" className=" flex flex-col relative m-[10px] justify-between">
+        <div className="absolute right-0 top-0 ">
+            <div onClick={() => Setshowlogin(false)}>icon</div>
+        </div>
+        <div className="flex flex-col my-6 mx-3 justify-between">
+          <div className="mt-2 mx-3 text-3xl font-bold">
+            <h1>New Here?</h1>
+            <h1>Create an Account.</h1>
+          </div>
+          <div className="flex flex-col justify-between my-2 py-5 px-2 w-[350px]  ">
+            <input type="text" placeholder="Full Name"  className="border p-1 my-2"/>
+            <input type="email" placeholder="Email Address" className="border p-1 my-2"/>
+            <input type="password" placeholder="Password" className="border p-1 my-2"/>
+            <input type="password" placeholder="Confirm Password" className="border p-1 my-2"/>
+            <button className="bg-[#001d35] text-white p-1 my-2">Sign up</button>
+          </div>
+
+          <span className="mx-3">Already have an account? <span onClick={()=>Setcurrentstate("SignIn")}>Sign In</span> </span>
+            {/* <div>{current}</div> */}
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default SignUp
