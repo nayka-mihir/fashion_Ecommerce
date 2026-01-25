f356cbd88ccaf509b966cd2d8c468693532fa8c8
nayka-mihir <mihirnayka786@gmail.com>
2026-01-25 17:07:21 +0530

    Add offers on product detail

---
+++ src/components/productdetail/Productdata/ProductInfo/OffersAndDelivery.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function OffersAndDelivery() {
+  return (
+    <div>
+        <div className='py-2 px-3 items-center'>
+            <div className='my-2'>Save extra with these offers</div>
+            <div className='flex py-2 my-2='><div className='p-2 border mx-2'>%</div><div><b>Get EXTRA 10% Cashback</b><div>On purchases above ₹499. T&C apply</div></div></div>
+            <div className='py-2  my-2'><span className='p-2 border mx-2'>+</span><span><b>Buy 2 for ₹999</b></span><span>Auto applied offer</span></div>
+        </div>
+    </div>
+  )
+}
+
+export default OffersAndDelivery
