abb95a5f050a6e89952d4d9d04a6e5d4e00897d7
nayka-mihir <mihirnayka786@gmail.com>
2026-01-25 17:06:28 +0530

    add and design product Detail Headerfile

---
+++ src/components/productdetail/Productdata/ProductInfo/HeaderSection.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function HeaderSection() {
+  return (
+    <div>
+        <div className='flex flex-col'>
+            <div className='text-lg font-medium'>Bewakoof®</div>
+            <div className='text-xl font-medium'>Women's Orange Queen Bee Graphic Printed Oversized T-shirt</div>
+        </div>
+    </div>
+  )
+}
+
+export default HeaderSection
