1d6f0fab1e03fea2037a1f122a1a778281519de1
nayka-mihir <mihirnayka786@gmail.com>
2026-01-26 22:37:57 +0530

    change validation form

--- src/components/utils/validators.js
+++ src/components/utils/validators.js
@@ -1,11 +1,9 @@
 export const validateEmail = (email) =>{
-    const pattern = /^[^\\s@]+@[^\\s@]+\\.[^\\s@]+$/;
-
-    pattern.test(email);
+    /^[^\\s@]+@[^\\s@]+\\.[^\\s@]+$/.test(email);
 }
 
 export const validatePassword =(password)=>{
-    const Passwordpattern = password.length >= 8 && 
+    password.length >= 8 && 
     /[A-Z]/.test(password) &&
-    /[0-9]/.test(password); 
+    /[0-9]/.test(password) 
 }
