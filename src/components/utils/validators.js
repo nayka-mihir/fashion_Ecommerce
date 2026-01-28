02b01981ee60ef918329b379b5cddde68602d5ed
nayka-mihir <mihirnayka786@gmail.com>
2026-01-28 22:27:57 +0530

    get proper validation

--- src/components/utils/validators.js
+++ src/components/utils/validators.js
@@ -1,9 +1,10 @@
-export const validateEmail = (email) =>{
-    /^[^\\s@]+@[^\\s@]+\\.[^\\s@]+$/.test(email);
-}
+export const validateEmail = (email) => {
+  return /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i.test(email);
+};
+
 
 export const validatePassword =(password)=>{
-    password.length >= 8 && 
-    /[A-Z]/.test(password) &&
-    /[0-9]/.test(password) 
+    return password.length >= 8 && 
+            /[A-Z]/.test(password) &&
+            /[0-9]/.test(password) 
 }
