d2025e749a210ac30bbe96485567284b528558f7
nayka-mihir <mihirnayka786@gmail.com>
2026-01-28 22:28:58 +0530

    set proper data fetch from sign up

--- src/components/Login_Sign/SignIn.jsx
+++ src/components/Login_Sign/SignIn.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Webicon } from '../../assets/assets'
 import InputField from '../common/InputField'
+import { loginUser } from '../../api/authService'
+import { validateEmail } from '../utils/validators'
 
 function SignIn({Setshowlogin, current,Setcurrentstate}) {
+
+  const [form , setForm] = useState({email:"", password:""});
+  const [error, setError] = useState('');
+
+  const handleChange = (e)=>
+    setForm({...form,[e.target.name]:e.target.value});
+  
+  const handleSubmit =async (e)=>{
+    e.preventDefault();
+
+    if (!validateEmail(form.email)) return setError("Invalid email");
+    if (!form.password) return setError("Password required");
+
+    const res = await loginUser(form);
+
+    if (!res.success) setError(res.message);
+    else alert("Login Successful ✅");
+
+  }
   return (
      <div className=" absolute z-1 items-center bg-[#fff] top-20 left-150 border">
       <form action="" className=" flex flex-col relative m-[10px] justify-between">
@@ -17,12 +38,13 @@ function SignIn({Setshowlogin, current,Setcurrentstate}) {
             <h1>Please Sign In.</h1>
           </div>
             <h3 className='mx-3 mt-2'>Every Login is Required for Your Security</h3>
+            {error && <p className="text-red-500 text-sm mb-3">{error}</p>}
           <div className="flex flex-col justify-between my-2 py-5 px-2 w-[350px]  ">
             {/* <input type="email" placeholder="Email Address" className="border p-1 my-2"/> */}
             {/* <input type="password" placeholder="Password" className="border p-1 my-2"/> */}
-            <InputField label="Email" name="Email" type="email" placeholder="Email Address"/>
-            <InputField label="Password" name="password" type="password" placeholder="Password"/>
-            <button className="bg-[#001d35] text-white p-1 my-2">Sign In</button>
+            <InputField label="Email" name="Email" type="email" placeholder="Email Address" onchange={handleChange}/>
+            <InputField label="Password" name="password" type="password" placeholder="Password" onchange={handleChange}/>
+            <button className="bg-[#001d35] text-white p-1 my-2" name='submit'>Sign In</button>
           </div>
 
           <span className="mx-3">New here ? <span onClick={()=>Setcurrentstate("Signup")}> Create a account</span> </span>
