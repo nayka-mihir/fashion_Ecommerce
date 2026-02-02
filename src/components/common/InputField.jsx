b95177d0045e8433433e1b012ded35157d11c7ce
nayka-mihir <mihirnayka786@gmail.com>
2026-02-02 10:48:54 +0530

    remove label

--- src/components/common/InputField.jsx
+++ src/components/common/InputField.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function InputField({label ,className=" ", ...props}) {
   return (
     <div>
-        <label className=''>{label}</label><br/>
+        {/* <label className=''>{label}</label><br/> */}
         <input {...props} className={`border w-[330px] ${className} px-3 py-2 rounded focus:outline-none focus:ring-2  focus:ring-blue-500`} />
     </div>
   )
